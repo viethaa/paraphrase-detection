8451348d3fd29bdac47bcbfdcff589aa8983c8a3
Coru <corucoru001@gmail.com>
2025-09-20 10:04:07 +0700

    Update main.py

--- main.py
+++ main.py
@@ -7,3 +7,10 @@ import matplotlib.pyplot as plt
 import time
 
 torch.manual_seed(77)
+
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+    print("Using CUDA device:", torch.cuda.get_device_name(0))
+else:
+    device = torch.device("cpu")
+    print("CUDA not available, using CPU.")
